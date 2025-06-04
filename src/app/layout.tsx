b89785eeef151954dc913ec601d59258022cefea
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-06-04 20:01:29 +0400

    add main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
+import { Stack } from "@mui/material";
 import "./globals.css";
+import Navbar from "@/components/_common/Navbar";
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+})
 
 export default function RootLayout({
   children,
@@ -7,8 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className={montserrat.className} style={{ margin: 0 }}>
+        <Stack>
+          <Navbar />
+        </Stack>
+        <Stack component={'main'} top={'80px'} position={'absolute'}>
+          {children}
+        </Stack>
       </body>
     </html>
   );
