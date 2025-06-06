55b23d1ab89d367fa5da0242c4ccb46c0df989d9
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-06-06 14:37:45 +0400

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ export const metadata: Metadata = {
   title: 'Home Page | Timeless Elegance'
 }
 
-export default function Home() {
+export default async function Home() {
   return <MainPageCom />
 }
